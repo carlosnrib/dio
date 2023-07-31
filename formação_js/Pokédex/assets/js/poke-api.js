5b55d83e63f0b58230eae11242842174a4355bef
Carlos Ribeiro <carlosnribeiro3@gmail.com>
2023-07-31 10:03:26 -0300

    Add files via upload

---
+++ formação_js/Pokédex/assets/js/poke-api.js
@@ -0,0 +1,35 @@
+
+
+const PokeApi = {}
+
+function convertPokeApiDetailoPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.name = pokeDetail.name
+    pokemon.number = pokeDetail.order
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    
+    return pokemon
+}
+
+PokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailoPokemon)
+}
+
+PokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(PokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+}
